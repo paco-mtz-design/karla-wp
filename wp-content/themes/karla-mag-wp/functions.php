e41f7db3d7c2a1ed1c4c63a97d596081be13a666
pacster <martinez@raidho.mx>
2016-12-04 18:32:30 -0600

    Se activa el uso de featured Images desde FUNCTIONS.PHP

---
+++ wp-content/themes/karla-mag-wp/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+if (function_exists('add_theme_support'))
+{
+	// Add Menu Support
+	add_theme_support('menus');
+
+	// Add Thumbnail Theme Support
+	add_theme_support('post-thumbnails');
+}
+
+?>
