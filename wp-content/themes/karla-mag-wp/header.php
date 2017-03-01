a22b91e262cd08f2762f22efdc14f3f6ce1816a3
pacster <martinez@raidho.mx>
2017-03-01 00:32:55 -0600

    Se agrega tracking code de Google Analytics

--- wp-content/themes/karla-mag-wp/header.php
+++ wp-content/themes/karla-mag-wp/header.php
@@ -18,6 +18,16 @@
 </head>
 <body <?php body_class(); ?>>
 
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-92821664-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 	<header id="header">
 		<h2 class="neuton light"><a href="<?php echo home_url(); ?>" title="<?php wp_title(''); ?>">Karla Martínez<span class="light_grey">.com</span></a></h2>
 		<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_id' => 'menu' ) ); ?>
