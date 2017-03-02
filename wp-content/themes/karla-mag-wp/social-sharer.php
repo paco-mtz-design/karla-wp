3dfe2e81d292c3005cd0dede17c7bd1a61eb98b1
pacster <martinez@raidho.mx>
2017-03-01 21:28:03 -0600

    Fixes al social-sharer - Se elimina Pinterest y se agrega twitter - Se corrige problema con acomodo de botones en responsivo (para que ya no se empalmen)

--- wp-content/themes/karla-mag-wp/social-sharer.php
+++ wp-content/themes/karla-mag-wp/social-sharer.php
@@ -1,4 +1,4 @@
 <div class="social_share">
 	<a class="social_facebook" href="#">Compartir en Facebook</a>
-	<a class="social_pinterest" href="#">Compartir en Pinterest</a>
+	<a class="social_twitter" href="#">Compartir en Twitter</a>
 </div>
